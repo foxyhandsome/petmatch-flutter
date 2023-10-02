9fef662809388d932c1f9a4fe7e995bf348b46d0
kaoukung <kaou913@gmail.com>
2023-10-02 23:27:50 +0700

    ff

--- lib/authenticationsScreen/test.dart
+++ lib/authenticationsScreen/test.dart
@@ -20,7 +20,7 @@ class _testState extends State<test> {
                 iconSize: 50.0,
                 items: const [
 
-                  DropdownMenuItem(child: Text("1"),value: "1",),
+                  DropdownMenuItem(child: Text("11"),value: "1",),
                   DropdownMenuItem(child: Text("2"),value: "2",),
                   DropdownMenuItem(child: Text("3"),value: "3",),
                   DropdownMenuItem(child: Text("4"),value: "4",),
