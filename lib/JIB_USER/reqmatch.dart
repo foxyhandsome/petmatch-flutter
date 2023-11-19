c5a658674d5163d097b59c8e11f86bd1e5c3588f
flame123flame <flame.tana@gmail.com>
2023-11-19 16:34:50 +0700

    get pet

--- lib/JIB_USER/reqmatch.dart
+++ lib/JIB_USER/reqmatch.dart
@@ -1,9 +1,16 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:petmatch/JIB_USER/allpet.dart';
 import 'package:petmatch/JIB_USER/history.dart';
 import 'package:petmatch/JIB_USER/home.dart';
 import 'package:petmatch/JIB_USER/realuser.dart';
 import 'package:petmatch/authenticationsScreen/navigationbar/profileother.dart';
+
+import '../constant/domain.dart';
+import '../model/pet.model.dart';
 
 class Reqmatch extends StatefulWidget {
   const Reqmatch({super.key});
@@ -13,113 +20,194 @@ class Reqmatch extends StatefulWidget {
 }
 
 class _ReqmatchState extends State<Reqmatch> {
+  final dio = Dio();
+  String? idUser;
+  static FlutterSecureStorage storageToken = new FlutterSecureStorage();
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  List<Pet> pets = [];
+  Future<void> getData() async {
+    try {
+      pets = [];
+      // idUser = await storageToken.read(key: 'id_user');
+      idUser = "20";
+      final response = await dio.post(url_api + '/match/pet-ticket', data: {
+        "id_user": int.parse(idUser!),
+        "id_userhome": int.parse(idUser!),
+        "id_userguest": int.parse(idUser!)
+      });
+      if (response.statusCode == 201) {
+        final responseData = response.data;
+        for (var element in responseData) {
+          pets.add(Pet(
+              provinceName: element["province_name"],
+              nameDistrict: element["name_district"],
+              nameSubdistrict: element["name_subdistrict"],
+              username: element["username"],
+              typeSkin: element["type_skin"],
+              typeBlood: element["type_blood"],
+              password: element["password"],
+              nameBreed: element["name_breed"],
+              information: element["information"],
+              idTypeuser: element["id_typeuser"],
+              idSubdistrict: element["id_subdistrict"],
+              idDistrict: element["id_district"],
+              contact: element["contact"],
+              agePet: element["age_pet"],
+              healthPet: element["health_pet"],
+              idBlood: element["id_blood"],
+              idBreed: element["id_breed"],
+              idPet: element["id_pet"],
+              idSkin: element["id_skin"],
+              idUser: element["id_user"],
+              namePet: element["name_pet"],
+              picturePet: element["picture_pet"],
+              sexPet: element["sex_pet"]));
+        }
+        setState(() {});
+      } else {
+        print('Request failed with status: ${response.statusCode}');
+      }
+    } catch (e) {
+      // Handle any exceptions that may occur during the request
+      print('Error: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 239, 83, 80),
-        elevation: 0,
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back, color: Colors.black),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 239, 83, 80),
+          elevation: 0,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          title: Text(
+            "ข้อเสนอการจับคู่",
+            style: TextStyle(color: Colors.black),
+          ),
+          actions: [],
         ),
-        title: Text(
-          "ข้อเสนอการจับคู่",
-          style: TextStyle(color: Colors.black),
-        ),
-        actions: [],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 10,
-            ),
-            Center(
-              child: Container(
-                width: 350, // ปรับความกว้างตามที่ต้องการ
-                child: ListTile(
-                  contentPadding: EdgeInsets.all(
-                      10), // ปรับระยะห่างของเนื้อหาภายใน ListTile
-                  tileColor: Colors.white, // สีพื้นหลังของ ListTile
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(
-                        color: Color.fromARGB(255, 239, 83, 80),
-                        width: 1), // กำหนดสีและความกว้างของเส้นกรอบ
-                    borderRadius:
-                        BorderRadius.circular(10), // กำหนดรูปร่างของกรอบ
-                  ),
-                  leading: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => profileother()),
-                      );
-                    },
-                    child: CircleAvatar(
-                      radius: 30,
-                      backgroundImage: NetworkImage(
-                          'https://img.kapook.com/u/2022/Jarosphan/Pet/Dog/159/p01.jpg'),
+        body: SingleChildScrollView(
+            child: pets.length > 0
+                ? Column(
+                    children: List.generate(
+                      pets.length, // Assuming you have only one item in the list, adjust accordingly
+                      (index) => SizedBox(
+                        child: Container(
+                          margin: EdgeInsets.all(10),
+                          child: Center(
+                            child: Container(
+                              child: ListTile(
+                                contentPadding: EdgeInsets.all(
+                                    10), // ปรับระยะห่างของเนื้อหาภายใน ListTile
+                                tileColor:
+                                    Colors.white, // สีพื้นหลังของ ListTile
+                                shape: RoundedRectangleBorder(
+                                  side: BorderSide(
+                                      color: Color.fromARGB(255, 239, 83, 80),
+                                      width:
+                                          1), // กำหนดสีและความกว้างของเส้นกรอบ
+                                  borderRadius: BorderRadius.circular(
+                                      10), // กำหนดรูปร่างของกรอบ
+                                ),
+                                leading: GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) => profileother()),
+                                    );
+                                  },
+                                  child: CircleAvatar(
+                                    radius: 30,
+                                    backgroundImage: MemoryImage(base64Decode(
+                                        "${pets[index].picturePet}")),
+                                  ),
+                                ),
+                                title: Row(
+                                  children: <Widget>[
+                                    Text(
+                                      "${pets[index].namePet}",
+                                      style: TextStyle(
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ],
+                                ),
+                                subtitle: Row(
+                                  children: <Widget>[
+                                    Icon(Icons.location_on,
+                                        color: Colors.grey), // ไอคอนของที่อยู่
+                                    SizedBox(
+                                        width:
+                                            4), // ระยะห่างระหว่างไอคอนกับข้อความ
+                                    Container(
+                                      width: 150,
+                                      child: Text(
+                                        "${pets[index].nameSubdistrict},${pets[index].nameDistrict}",
+                                        style: TextStyle(
+                                            overflow: TextOverflow.clip),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                trailing: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        border: Border.all(
+                                          color: Color.fromARGB(255, 0, 128, 0),
+                                          width: 2.0,
+                                        ),
+                                      ),
+                                      child: IconButton(
+                                        icon:
+                                            Icon(Icons.check), // ไอคอนของยอมรับ
+                                        color: Color.fromARGB(255, 0, 128, 0),
+                                        onPressed: () {},
+                                      ),
+                                    ),
+                                    SizedBox(
+                                        width:
+                                            15.0), // ระยะห่างระหว่างกรอบวงกลม
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        border: Border.all(
+                                          color: Color.fromARGB(255, 255, 0, 0),
+                                        ),
+                                      ),
+                                      child: IconButton(
+                                        icon:
+                                            Icon(Icons.close), // ไอคอนของปฏิเสธ
+                                        color: Color.fromARGB(255, 255, 0, 0),
+                                        onPressed: () {},
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                  title: Row(
-                    children: <Widget>[
-                      Text(
-                        "เขี้ยว",
-                        style: TextStyle(
-                            fontSize: 22, fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                  subtitle: Row(
-                    children: <Widget>[
-                      Icon(Icons.location_on,
-                          color: Colors.grey), // ไอคอนของที่อยู่
-                      SizedBox(width: 4), // ระยะห่างระหว่างไอคอนกับข้อความ
-                      Text("สีลม, บางรัก"),
-                    ],
-                  ),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: Color.fromARGB(255, 0, 128, 0),
-                            width: 2.0,
-                          ),
-                        ),
-                        child: IconButton(
-                          icon: Icon(Icons.check), // ไอคอนของยอมรับ
-                          color: Color.fromARGB(255, 0, 128, 0),
-                          onPressed: () {},
-                        ),
-                      ),
-                      SizedBox(width: 15.0), // ระยะห่างระหว่างกรอบวงกลม
-                      Container(
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: Color.fromARGB(255, 255, 0, 0),
-                          ),
-                        ),
-                        child: IconButton(
-                          icon: Icon(Icons.close), // ไอคอนของปฏิเสธ
-                          color: Color.fromARGB(255, 255, 0, 0),
-                          onPressed: () {},
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+                  )
+                : Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: Center(child: Text("ไม่มี")),
+                  )));
   }
 }
--- lib/model/pet.model.dart
+++ lib/model/pet.model.dart
@@ -1,8 +1,4 @@
 class Pet {
-  int? idPet;
-  String? picturePet;
-  String? sexPet;
-  String? healthPet;
   String? namePet;
   int? agePet;
   int? idSkin;
@@ -11,6 +7,7 @@ class Pet {
   int? idBreed;
   String? typeSkin;
   String? typeBlood;
+  String? nameBreed;
   String? username;
   String? password;
   String? information;
@@ -18,14 +15,25 @@ class Pet {
   int? idDistrict;
   int? idSubdistrict;
   int? idTypeuser;
-  String? nameBreed;
+  String? nameDistrict;
+  String? provinceName;
+  String? nameSubdistrict;
+  String? picturePet;
+  int? idMatch;
+  int? idUserhome;
+  int? idPethome;
+  int? idUserguest;
+  int? idPetguest;
+  int? matchUserguest;
+  int? matchUserguestDeny;
+  int? matchUserhome;
+  int? matchDislike;
+  int? idPet;
+  String? sexPet;
+  String? healthPet;
 
   Pet(
-      {this.idPet,
-      this.picturePet,
-      this.sexPet,
-      this.healthPet,
-      this.namePet,
+      {this.namePet,
       this.agePet,
       this.idSkin,
       this.idBlood,
@@ -33,6 +41,7 @@ class Pet {
       this.idBreed,
       this.typeSkin,
       this.typeBlood,
+      this.nameBreed,
       this.username,
       this.password,
       this.information,
@@ -40,13 +49,24 @@ class Pet {
       this.idDistrict,
       this.idSubdistrict,
       this.idTypeuser,
-      this.nameBreed});
+      this.nameDistrict,
+      this.provinceName,
+      this.nameSubdistrict,
+      this.picturePet,
+      this.idMatch,
+      this.idUserhome,
+      this.idPethome,
+      this.idUserguest,
+      this.idPetguest,
+      this.matchUserguest,
+      this.matchUserguestDeny,
+      this.matchUserhome,
+      this.matchDislike,
+      this.idPet,
+      this.sexPet,
+      this.healthPet});
 
   Pet.fromJson(Map<String, dynamic> json) {
-    idPet = json['id_pet'];
-    picturePet = json['picture_pet'];
-    sexPet = json['sex_pet'];
-    healthPet = json['health_pet'];
     namePet = json['name_pet'];
     agePet = json['age_pet'];
     idSkin = json['id_skin'];
@@ -55,6 +75,7 @@ class Pet {
     idBreed = json['id_breed'];
     typeSkin = json['type_skin'];
     typeBlood = json['type_blood'];
+    nameBreed = json['name_breed'];
     username = json['username'];
     password = json['password'];
     information = json['information'];
@@ -62,15 +83,26 @@ class Pet {
     idDistrict = json['id_district'];
     idSubdistrict = json['id_subdistrict'];
     idTypeuser = json['id_typeuser'];
-    nameBreed = json['name_breed'];
+    nameDistrict = json['name_district'];
+    provinceName = json['province_name'];
+    nameSubdistrict = json['name_subdistrict'];
+    picturePet = json['picture_pet'];
+    idMatch = json['id_match'];
+    idUserhome = json['id_userhome'];
+    idPethome = json['id_pethome'];
+    idUserguest = json['id_userguest'];
+    idPetguest = json['id_petguest'];
+    matchUserguest = json['match_userguest'];
+    matchUserguestDeny = json['match_userguest_deny'];
+    matchUserhome = json['match_userhome'];
+    matchDislike = json['match_dislike'];
+    idPet = json['id_pet'];
+    sexPet = json['sex_pet'];
+    healthPet = json['health_pet'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id_pet'] = this.idPet;
-    data['picture_pet'] = this.picturePet;
-    data['sex_pet'] = this.sexPet;
-    data['health_pet'] = this.healthPet;
     data['name_pet'] = this.namePet;
     data['age_pet'] = this.agePet;
     data['id_skin'] = this.idSkin;
@@ -79,6 +111,7 @@ class Pet {
     data['id_breed'] = this.idBreed;
     data['type_skin'] = this.typeSkin;
     data['type_blood'] = this.typeBlood;
+    data['name_breed'] = this.nameBreed;
     data['username'] = this.username;
     data['password'] = this.password;
     data['information'] = this.information;
@@ -86,7 +119,22 @@ class Pet {
     data['id_district'] = this.idDistrict;
     data['id_subdistrict'] = this.idSubdistrict;
     data['id_typeuser'] = this.idTypeuser;
-    data['name_breed'] = this.nameBreed;
+    data['name_district'] = this.nameDistrict;
+    data['province_name'] = this.provinceName;
+    data['name_subdistrict'] = this.nameSubdistrict;
+    data['picture_pet'] = this.picturePet;
+    data['id_match'] = this.idMatch;
+    data['id_userhome'] = this.idUserhome;
+    data['id_pethome'] = this.idPethome;
+    data['id_userguest'] = this.idUserguest;
+    data['id_petguest'] = this.idPetguest;
+    data['match_userguest'] = this.matchUserguest;
+    data['match_userguest_deny'] = this.matchUserguestDeny;
+    data['match_userhome'] = this.matchUserhome;
+    data['match_dislike'] = this.matchDislike;
+    data['id_pet'] = this.idPet;
+    data['sex_pet'] = this.sexPet;
+    data['health_pet'] = this.healthPet;
     return data;
   }
 }
